fbc38cc89beaf93c863fea6ee12693da107f29d1
AragonJ13 <jeanaragon0@gmail.com>
2023-11-29 20:43:38 -0800

    updated game

--- README.md
+++ README.md
@@ -47,20 +47,53 @@ As a developer, you are tasked with creating a treasure hunt game. The user will
 
 ### 📚 User Stories
 
-- As a user, I can see a page with a three by three grid board game with a question mark in each square.
--mapped over array in state to dynamically render 9 squares.
--styled the board 
--passed value of state to sqaure
--style square
-- As a user, when I click on one of the question marks, an alert appears with the index position of that question mark in the array.
-- As a user, when I click on one of the question marks, a tree emoji appears instead of the alert.
-- As a user, if I select the winning square, the question mark will become a treasure emoji. If I select the losing square, the question mark will become a bomb emoji.
-- As a user, I can click on a “Play Again” button that will restart the game.
-- As a user, I can see a counter that shows how many guesses are remaining. The counter starts at five and decrements by one every time I click on a square that is neither the treasure nor the bomb.
-- As a user, I can see a message stating that I won the game by selecting the square that contains the treasure.
-- As a user, I can see a message stating that I lost the game by selecting the square that contains the bomb.
-- As a user, I cannot continue to play the game after I win or lose.
-- As a user, I can see a message informing me that I lost the game when I run out of turns (the counter reaches zero).
+-✅As a user, I can see a page with a three by three grid board game with a question mark in each square.
+
+    -branch: game-board
+    -mapped over array in state to dynamicallt render 9 squares
+    -styled the board
+    -passed value of state to square
+    -styled square
+
+✅ As a user, when I click on one of the question marks, an alert appears with the index position of that question mark in the array.
+
+    -added onClick to square div
+    -created handleClick method to square
+    -passed the index to square and handleClick
+    -added handleSquareClick to App.js
+    -passes handleSqareClick to square to get the index back to App.js(using functional props)
+
+✅ As a user, when I click on one of the question marks, a tree emoji appears instead of the alert.
+    -Made a copy of board using spread operator 
+    -updated value of square at particular index to be a tree 
+
+✅ As a user, if I select the winning square, the question mark will become a treasure emoji. If I select the losing square, the question mark will become a bomb emoji.
+
+    - Made a conditional statement in my handleSquareClick that if clicked on treasureLocation or bombLocation would change from '?' to diamond or bomb.
+
+✅ As a user, I can click on a “Play Again” button that will restart the game.
+
+    - Made variable named restartGame and button that change back all boards to '?' and randomize the squares again.
+
+✅ As a user, I can see a counter that shows how many guesses are remaining. The counter starts at five and decrements by one every time I click on a square that is neither the treasure nor the bomb.
+
+    -Made a varibale = count that ony had 5 chances in the useState for the user. Added that to the else statement in my conditional that subtracted every click from updatedBoard that would rech 0 if they did not find treasure/bomb.
+
+✅As a user, I can see a message stating that I won the game by selecting the square that contains the treasure.
+
+    -Added an alert so if user clicked medal, user would see a pop up saying you win. 
+
+✅ As a user, I can see a message stating that I lost the game by selecting the square that contains the bomb.
+
+    -Added an alert so if user clicked bomb, user would see a pop up saying you lose.
+
+✅As a user, I cannot continue to play the game after I win or lose.
+
+    -Made varibale = gameOver that equaled false in my state varibales. Attched the gameOver variable to conditonal statements and set them to true if they did click on treasure/bomb or if they ran out of turns. 
+
+✅As a user, I can see a message informing me that I lost the game when I run out of turns (the counter reaches zero).
+
+    -Learned and made a nested if statement that if count reached 1 than it would alert the user that they have ran out of turns and game over and can play again 
 
 ### 🏔 Stretch Goals
 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import Square from "./components/Square";
 import "./App.css";
+import emptyocean from './emptyocean.jpg'
+import leviathan from './leviathan.jpg'
+import entrnace from './entrance.jpg'
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -13,87 +16,87 @@ const App = () => {
     "?",
     "?",
     "?",
-  ])
+  ]);
 
-  const [count, setCount] = useState()  
-  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [count, setCount] = useState(5);
 
-  const [bombLocation, setBombLocation] =
-  useState(Math.floor(Math.random() * board.length))
+  const [gameOver, setGameOver] = useState(false);
+
+  const [treasureLocation, setTreasureLocation] = useState(
+    Math.floor(Math.random() * board.length)
+  );
+
+  const [bombLocation, setBombLocation] = useState(
+    Math.floor(Math.random() * board.length)
+  );
 
   const restartGame = () => {
-    setBoard([
-        "?",
-        "?",
-        "?",
-        "?",
-        "?",
-        "?",
-        "?",
-        "?",
-        "?",
-      ])
-        setTreasureLocation(Math.floor(Math.random() * board.length))
-        setBombLocation(Math.floor(Math.random() * board.length))
-
-  
-    }
-  
-
+    setBoard(["?", "?", "?", "?", "?", "?", "?", "?", "?"]);
+    setTreasureLocation(Math.floor(Math.random() * board.length));
+    setBombLocation(Math.floor(Math.random() * board.length));
+    setCount(5);
+    setGameOver(false);
+  };
 
   const handleSquareClick = (clickedSquareIndex) => {
-    let updatedBoard = [...board]
+    if (gameOver) {
+      return;
+    }
+    //varibale holding copy of current state
+    // [...] <-- spread operator makes copy of whatever we tell it too in this case board.
+    let updatedBoard = [...board];
     // set condition for if treasure location is same as clikced squares index show treasure
-    if(clickedSquareIndex === treasureLocation) {
+    if (clickedSquareIndex === treasureLocation) {
       // then reassign state value at the index to treasure emoji
-      updatedBoard[clickedSquareIndex] = "🥇"
-      alert("You win!")
+      updatedBoard[clickedSquareIndex] =<img src={entrnace} alt= "entrance" />;;
+      alert("You found the entrance to Atlantis! History will remember your name !");
+      setGameOver(true);
       //setBoard(updatedBoard)
     } else if (clickedSquareIndex === bombLocation) {
-      updatedBoard[clickedSquareIndex] = "💣"
-      alert("You lose!")
+      updatedBoard[clickedSquareIndex] = <img src={leviathan} alt= "monster" />;
+      alert("Your ship/sub was destroyed by the Leviathan! Better luck next time!");
+      setGameOver(true);
       //setBoard(updatedBoard)
     } else {
-      //use index from clickedSquareIndex to update the clicked squares 
-      updatedBoard[clickedSquareIndex] = "🌲"
-      setCount(count - 1)
+      //use index from clickedSquareIndex to update the clicked squares
+      updatedBoard[clickedSquareIndex] = <img src={emptyocean} alt= "ocean" />;
+      setCount(count - 1);
       //update state with the new board
-      
-  }
-  setBoard(updatedBoard)
-  }
-    
+      if (count === 1) {
+        alert(
+          "Game Over!! You're ship/sub sunk to the bottom of the ocean. Would you like to play again?"
+        );
+        setGameOver(true);
+      }
+    }
 
-  
-
-    
+    setBoard(updatedBoard);
+  };
 
   return (
     <>
-      <h1>Treasure Hunt Game</h1>
+      <h1>Searching For Atlantis</h1>
+
       <div className="board">
         {board.map((value, index) => {
-          console.log(value, index)
+          console.log(value, index);
           return (
-          <Square 
-          value={value} 
-          index={index} 
-          handleSquareClick ={handleSquareClick}
-          />
-          )
+            <Square
+              value={value}
+              index={index}
+              handleSquareClick={handleSquareClick}
+            />
+          );
         })}
       </div>
-      <p className="clicker" onClick={setCount}> Attemps{count}</p>
-      <button className="buttons" onClick={restartGame}
+
+      <p className="countdown">{count} Attemps Left</p>
       
-      
-      >Restart Game</button>
+      <button className="buttons" onClick={restartGame}>
+        Restart Game
+      </button>
 
-
-
-
-
-      
+      <footer> Made by Jean Aragon</footer>
     </>
   );
 };
